e0985dffd875c430ed9ca5a135d4f87788d62854
ismail036 <90846601+ismail036@users.noreply.github.com>
2024-03-08 00:13:21 +0300

    first commit

---
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by İsmail Parlak on 7.03.2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
