0e09133fc844075fb207349d130a013bc32f5278
ismail036 <90846601+ismail036@users.noreply.github.com>
2024-03-08 11:13:42 +0300

    Creating Table’s HeaderView

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by İsmail Parlak on 8.03.2024.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
