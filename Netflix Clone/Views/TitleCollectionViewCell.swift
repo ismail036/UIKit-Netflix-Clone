954118a6b52b227b6fe0e1b950fc01cce7b2f6d8
ismail036 <90846601+ismail036@users.noreply.github.com>
2024-03-09 12:43:35 +0300

    Creating Custom CollectionViewCell

---
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by İsmail Parlak on 9.03.2024.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
