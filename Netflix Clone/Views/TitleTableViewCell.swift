21463562fd2cfeeba5f12b932088bdf4cda446a9
ismail036 <90846601+ismail036@users.noreply.github.com>
2024-03-12 15:53:34 +0300

    Creating custom TableViewCell from the upcoming table

--- Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -8,16 +8,55 @@
 import UIKit
 
 class TitleTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    static let identifier = "TitleTableViewCell"
+    
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titleLabel:UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titlesPosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titlesPosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        
+        applyConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private func applyConstraints() {
+        let titlesPosterUIImageViewConstaints = [
+            titlesPosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlesPosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor , constant: 15),
+            titlesPosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            titlesPosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(titlesPosterUIImageViewConstaints)
     }
-
+    
+    public func configure(with model: TitleViewModel){
+        guard let url = URL(string: model.posterURL) else {return}
+        titlesPosterUIImageView.sd_setImage(with: url,completed:nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
 }
