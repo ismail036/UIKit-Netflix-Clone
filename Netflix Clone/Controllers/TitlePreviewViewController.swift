74c23aaa5c268e158dabd1a2aeffa8097238c55f
ismail036 <90846601+ismail036@users.noreply.github.com>
2024-03-22 02:13:02 +0300

    Creating TitlePreviewViewController

---
+++ Netflix Clone/Controllers/TitlePreviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TitlePreviewViewController.swift
+//  Netflix Clone
+//
+//  Created by İsmail Parlak on 21.03.2024.
+//
+
+import UIKit
+
+class TitlePreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by İsmail Parlak on 22.03.2024.
+//
+
+import Foundation
